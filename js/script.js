e7db74c7396e96be70a6d90a40403101626dbccd
HenriqueMafort <hmafort@gmail.com>
2022-09-26 14:03:12 -0300

    erro de 500 reais

--- js/script.js
+++ js/script.js
@@ -8,9 +8,9 @@ function Calcular(){
     res.innerHTML = 'Preencha os valores'
     if(valor.length.value == 0 || valor == 0){
         alert('Preencha os dados corretamente para verificar os valores')
-    }else if (valor <= 500 ){
+    }else if (valor < 500 ){
         window.alert(`Você não tem direito a saque aniversário`)
-    }if (valor > 500 && valor <= 1000){
+    }if (valor >= 500 && valor <= 1000){
        res.innerHTML = (`Você receberá o valor de R$  ${(valor*0.4 + 50).toFixed(2)} Reais`)
     }if(valor > 1000.01 && valor <= 5000){
       res.innerHTML = (`Você receberá o valor de R$ ${(valor*0.3 + 150).toFixed(2)} Reais`)
